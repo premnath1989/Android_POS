3d28ea9062edd34560a8641ccd4ca093038eb348
Emi <emi@infoconnect.com.my>
2015-10-09 16:08:29 +0800

    First_Commit

---
+++ HLA/eSignature/SnapPhoto/Controller/CameraViewController.m
@@ -0,0 +1,43 @@
+//
+//  CameraViewController.m
+//  iMobile Planner
+//
+//  Created by Emi on 26/8/15.
+//  Copyright (c) 2015 InfoConnect Sdn Bhd. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+-(BOOL)shouldAutorotate
+{
+	return FALSE;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
